cf9e8cfa244401a0d1a523b5ae0233bccc174864
amine-hamzi <hamzimohammedhamzi@gmail.com>
2023-03-14 19:38:53 +0100

    Initial commit

---
+++ src/main/java/com/example/rabbitmqdemo/Runner.java
@@ -0,0 +1,24 @@
+package com.example.rabbitmqdemo;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(RabbitMqDemoApplication.TOPIC_EXCHANGE_NAME, "foo.bar.baz", "hello world");
+    }
+
+}
